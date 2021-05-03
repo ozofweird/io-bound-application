5fbaacb8f79dfb72beb374fbdf1552a4b0b3d48b
ozofweird <ozofweird@gmail.com>
2021-05-03 14:30:05 +0900

    :art: 내용 검색 추가

--- src/main/java/com/example/io/PostController.java
+++ src/main/java/com/example/io/PostController.java
@@ -36,5 +36,9 @@ public class PostController {
         return postRepository.findById(id).get();
     }
 
-    // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
+    // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글 (spring boot jpa like 구글링)
+    @GetMapping("/search")
+    public List<Post> findPostsByContent(@RequestParam String content) {
+        return postRepository.findByContentContains(content);
+    }
 }
